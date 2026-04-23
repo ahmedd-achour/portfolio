a45339d1497db15a9f8da83c6ac38e443e8f9308
ahmedd-achour <achourahmed709@gmail.com>
2026-04-23 16:55:09 +0100

    Initial portfolio commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { Home2Component } from './home2/home2.component';
+import { Home3Component } from './home3/home3.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'home2', component: Home2Component },
+    { path: 'home3', component: Home3Component },
+];
